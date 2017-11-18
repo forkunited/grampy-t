e835c60883864b40b55902c179c4e1b5f42f2768
Bill McDowell <wmcdowel@sherlock-ln03.stanford.edu>
2017-11-17 17:26:33 -0800

    Added test script

---
+++ run_fol_tests.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+TEST_DIR=src/test/py/gram/fol
+PWD=`pwd` 
+export PYTHONPATH=$PYTHONPATH:${PWD}/src/main/py/
+export PROVER9=<PATH TO PROVER BINARY>
+
+python ${TEST_DIR}/test_fol_rep.py
+#python ${TEST_DIR}/test_fol_data.py
+#python ${TEST_DIR}/test_fol_rules.py
+#python ${TEST_DIR}/test_fol_model.py
